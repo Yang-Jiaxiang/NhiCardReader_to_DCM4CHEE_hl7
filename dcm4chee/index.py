20248e2f81e3563559d1de60fa04285f10e217e0
Louis <81738019+Yang-Jiaxiang@users.noreply.github.com>
2023-03-09 02:15:31 +0800

    feat:use hl7 MLLP send OK

--- dcm4chee/index.py
+++ dcm4chee/index.py
@@ -1,8 +1,9 @@
 import requests
 import sys
 import os
-import socket
 from dotenv import load_dotenv
+import socket
+from hl7.client import CR, EB, SB, MLLPClient, MLLPException, mllp_send
 
 load_dotenv()
 dcm4cheeAPI=os.getenv("DCM4CHEE_API")
@@ -10,7 +11,7 @@ dcm4cheePORT=os.getenv("DCM4CHEE_POST")
 
 def apiGetWorkList():
     try:
-        response=requests.get(dcm4cheeAPI+"/WORKLIST/rs/workitems")
+        response=requests.get("http://"+dcm4cheeAPI+":8080/dcm4chee-arc/aets/WORKLIST/rs/workitems")
         if(response.status_code>=200 and response.status_code<=300):
             return {"status_code":200,"datas":response}
         else:
@@ -20,28 +21,6 @@ def apiGetWorkList():
 
 
 def apiPostWorkList(hl7_msg):
-    RECV_BUFFER = 4096
-    try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((dcm4cheeAPI, int(dcm4cheePORT)))
-            s.send(hl7_msg.encode())
-
-            # 等待dcm4chee回應，並接收回應
-            data = s.recv(RECV_BUFFER)
-
-            # 檢查dcm4chee的回應是否有錯誤訊息
-            if "ERR" in data.decode():
-                # 若dcm4chee的回應包含 "ERR"，則印出錯誤訊息
-                print(f"Error: {data.decode()}")
-            else:
-                # 若dcm4chee的回應不包含 "ERR"，則印出回應訊息
-                print(f"Response: {data.decode()}")
-    except socket.error as e:
-        # 捕捉socket錯誤，並印出錯誤訊息
-        print(f"Socket error: {e}")
-        sys.exit(1)
-    except Exception as e:
-        # 捕捉其他錯誤，並印出錯誤訊息
-        print(f"Error: {e}")
-        sys.exit(1)
-    s.close()
+    client = MLLPClient(dcm4cheeAPI, int(dcm4cheePORT))
+    result = client.send_message(hl7_msg)
+    print(result)
